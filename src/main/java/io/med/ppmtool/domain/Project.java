1e6618628b193c724c0438881b94b8fb7fec971a
Mohamed Zayd <abadmohamedzayd@gmail.com>
2020-12-22 21:29:40 +0100

    creating project repository - testing on h2-console

--- src/main/java/io/med/ppmtool/domain/Project.java
+++ src/main/java/io/med/ppmtool/domain/Project.java
@@ -1,4 +1,34 @@
 package io.med.ppmtool.domain;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@NoArgsConstructor
+@Data
+@Entity
 public class Project {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String identifier;
+    private String description;
+    private Date startDate;
+    private Date endDate;
+    private Date createdAt;
+    private Date updatedAt;
+
+    @PrePersist
+    protected void onCreate() {
+        this.createdAt = new Date();
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        this.updatedAt = new Date();
+    }
 }
---
+++ src/main/java/io/med/ppmtool/repositories/ProjectRepository.java
@@ -0,0 +1,9 @@
+package io.med.ppmtool.repositories;
+
+import io.med.ppmtool.domain.Project;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProjectRepository extends CrudRepository<Project, Long> {
+}
