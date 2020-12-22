ac37b8a5c4cbf9a2514f2ffc4b3da8f02df5b3ec
Mohamed Zayd <abadmohamedzayd@gmail.com>
2020-12-22 21:48:59 +0100

    saving a project via api

---
+++ src/main/java/io/med/ppmtool/services/ProjectService.java
@@ -0,0 +1,21 @@
+package io.med.ppmtool.services;
+
+import io.med.ppmtool.domain.Project;
+import io.med.ppmtool.repositories.ProjectRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProjectService {
+
+    private ProjectRepository projectRepository;
+
+    @Autowired
+    public ProjectService(ProjectRepository projectRepository) {
+        this.projectRepository = projectRepository;
+    }
+
+    public Project saveOrUpdateProject(Project project) {
+        return projectRepository.save(project);
+    }
+}
---
+++ src/main/java/io/med/ppmtool/web/ProjectController.java
@@ -0,0 +1,28 @@
+package io.med.ppmtool.web;
+
+import io.med.ppmtool.domain.Project;
+import io.med.ppmtool.services.ProjectService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/project")
+public class ProjectController {
+
+    private ProjectService projectService;
+
+    @Autowired
+    public ProjectController(ProjectService projectService) {
+        this.projectService = projectService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Project> createNewProject(@RequestBody Project project) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(projectService.saveOrUpdateProject(project));
+    }
+}
