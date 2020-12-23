80a0578a4d02728b4f3253584c4c0a5e15cc6f64
Mohamed Zayd <abadmohamedzayd@gmail.com>
2020-12-23 21:23:01 +0100

    project entity validation

--- src/main/java/io/med/ppmtool/domain/Project.java
+++ src/main/java/io/med/ppmtool/domain/Project.java
@@ -1,9 +1,12 @@
 package io.med.ppmtool.domain;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 import java.util.Date;
 
 @NoArgsConstructor
@@ -14,12 +17,28 @@ public class Project {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @NotBlank(message = "Project name is required")
     private String name;
+
+    @NotBlank(message = "Project identifier is required")
+    @Size(min = 4, max = 5, message = "Please use 4 to 5 characters")
+    @Column(unique = true, nullable = false)
     private String identifier;
+
+    @NotBlank(message = "Project description is required")
     private String description;
+
+    @JsonFormat(pattern = "yyyy-mm-dd")
     private Date startDate;
+
+    @JsonFormat(pattern = "yyyy-mm-dd")
     private Date endDate;
+
+    @JsonFormat(pattern = "yyyy-mm-dd")
     private Date createdAt;
+
+    @JsonFormat(pattern = "yyyy-mm-dd")
     private Date updatedAt;
 
     @PrePersist
